631fd0af32a4e312497fd5d659d8a0db070804ae
Christopher Higgs <higgsch@yahoo.com>
2017-01-08 18:26:46 -0700

    Cleanup

---
+++ FroggerSource/Frogger/Samples/makeCompile.bat
@@ -0,0 +1,15 @@
+IF NOT DEFINED VSDevEnv (
+SET VSDevEnv="true"
+echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat"
+
+cd "C:\Users\Wayne\Desktop\Notes\Personal\Frogger\FroggerSource\Frogger\Samples"
+)
+set arg=%1
+
+if exist ..\Debug\Frogger.exe (
+	nmake makefile compileSource fname=%arg%
+) else (
+	echo "Frogger is not compiled"
+)
+
--- FroggerSource/Frogger/Samples/makeclean.bat
+++ FroggerSource/Frogger/Samples/makeclean.bat
@@ -3,6 +3,6 @@ SET VSDevEnv="true"
 echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat"
 
-cd "C:\Users\Wayne\Desktop\Notes\Personal\Frogger"
+cd "C:\Users\Wayne\Desktop\Notes\Personal\Frogger\FroggerSource\Frogger\Samples"
 )
 nmake makefile clean
