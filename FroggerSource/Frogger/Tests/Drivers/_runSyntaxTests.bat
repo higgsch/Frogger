31e293197968e258960c94db2bc9ee7a14fa64dd
Christopher Higgs <higgsch@yahoo.com>
2017-07-06 13:30:11 -0600

    Added description of passing to syntax tests script

--- FroggerSource/Frogger/Tests/Drivers/_runSyntaxTests.bat
+++ FroggerSource/Frogger/Tests/Drivers/_runSyntaxTests.bat
@@ -1,16 +1,17 @@
 echo off
 
-call SyntaxChecking\runSyntaxTest.bat Arithmetic_Test Pass "Fail - Arithmetic Test"
-call SyntaxChecking\runSyntaxTest.bat Command_Test Pass "Fail - Command Test"
-call SyntaxChecking\runSyntaxTest.bat Function_Test Pass "Fail - Function Test"
-call SyntaxChecking\runSyntaxTest.bat If_Test Pass "Fail - If Test"
-call SyntaxChecking\runSyntaxTest.bat Syntax_Test Pass "Fail - Syntax Test"
-call SyntaxChecking\runSyntaxTest.bat Temp_Test Pass "Fail Temp Test"
+call SyntaxChecking\runSyntaxTest.bat Arithmetic_Test "Pass - Arithmetic Test" "Fail - Arithmetic Test"
+call SyntaxChecking\runSyntaxTest.bat Command_Test "Pass - Command Test" "Fail - Command Test"
+call SyntaxChecking\runSyntaxTest.bat Function_Test "Pass - Function Test" "Fail - Function Test"
+call SyntaxChecking\runSyntaxTest.bat If_Test "Pass - If Test" "Fail - If Test"
+call SyntaxChecking\runSyntaxTest.bat Syntax_Test "Pass - Syntax Test" "Fail - Syntax Test"
+call SyntaxChecking\runSyntaxTest.bat Temp_Test "Pass - Temp Test" "Fail Temp - Test"
 
-call SyntaxChecking\runSyntaxTest_PF.bat Stand_Alone Pass "Fail - Stand Alone"
-call SyntaxChecking\runSyntaxTest_PF.bat Stand_Alone_With_SCF Pass "Fail - Stand Alone with SCF"
-call SyntaxChecking\runSyntaxTest_PF.bat Single_UDF Pass "Fail - Single UDF"
-call SyntaxChecking\runSyntaxTest_PF.bat Multi_UDF Pass "Fail - Multi UDF"
+call SyntaxChecking\runSyntaxTest_PF.bat Stand_Alone "Pass - Stand Alone Function Test" "Fail - Stand Alone Function Test"
+call SyntaxChecking\runSyntaxTest_PF.bat Stand_Alone_With_SCF "Pass - Stand Alone with SCF Test" "Fail - Stand Alone with SCF Test"
+call SyntaxChecking\runSyntaxTest_PF.bat Single_UDF "Pass - Single UDF Test" "Fail - Single UDF Test"
+call SyntaxChecking\runSyntaxTest_PF.bat Multi_UDF "Pass - Multi UDF Test" "Fail - Multi UDF Test"
+
 
 pause
 
